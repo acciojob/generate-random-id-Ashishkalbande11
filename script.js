7e0d007326f53b7962f27de7c15297c55cb8bdba
Ashishkalbande11 <154236313+Ashishkalbande11@users.noreply.github.com>
2024-10-26 17:41:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function makeid(l) {
   // write your code here
+	 let s = "";
+  const str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < l; i++) {
+    let index = Math.floor(Math.random() * str.length);
+    s += str[index]; // Fix: use += to append the character
+  }
+  return s;
 }
 
+	
 // Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
+
